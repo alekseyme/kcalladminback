28dc6b35757835fe15331badd3abfd5cbbd0b9fe
Alexey <peneksem@gmail.com>
2021-10-29 10:57:19 +0300

    sanctum add api prefix

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,192.168.137.67,192.168.137.67:3000,192.168.137.67:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,admin.kolocall.com,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
@@ -47,5 +47,7 @@ return [
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
         'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
     ],
+
+    'prefix' => 'api',
 
 ];
